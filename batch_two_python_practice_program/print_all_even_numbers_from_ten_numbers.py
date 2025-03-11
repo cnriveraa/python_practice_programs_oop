87a278790543d25f8f383bf25de7828160c1b1fe
cnriveraa <rivera.cn18@gmail.com>
2025-03-12 07:53:59 +0800

    check if the number is even by dividing to 2, then will be incremented  by 1 if even

--- batch_two_python_practice_program/print_all_even_numbers_from_ten_numbers.py
+++ batch_two_python_practice_program/print_all_even_numbers_from_ten_numbers.py
@@ -6,3 +6,7 @@ even = 0
 # start a loop that will iterate 10 times
 for i in range(10):
     num = int(input("Enter a number: ")) n     # ask user to input a number
+
+    # check if the number is even
+    if num % 2 == 0:
+        even += 1         # if even, increment by 1
