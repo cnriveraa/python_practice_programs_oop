3c0318fc268f850054ed5ab0bc16226357fbaf50
cnriveraa <rivera.cn18@gmail.com>
2025-03-11 15:01:34 +0800

    ask user to input 2 numbers

---
+++ batch_two_python_practice_program/print_smaller_number_from_two_numbers.py
@@ -0,0 +1,5 @@
+#Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
+
+# ask user to input 2 numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
