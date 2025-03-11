c82452a5324ba3da6012668aadb33fd2cf004b72
cnriveraa <rivera.cn18@gmail.com>
2025-03-11 14:37:11 +0800

    print the current total sum of all the numbers entered so far

--- batch_one_python_practice_program/print_sum_of_ten_numbers.py
+++ batch_one_python_practice_program/print_sum_of_ten_numbers.py
@@ -5,5 +5,8 @@ sum = 0
 
 # start a loop that will iterate 10 times (from 0 to 9)
 for i in range(10): 
-    num = int(input("Enter a number: ")) # ask user to input a number
-    sum += num     # add the number to the 'sum' variable
+    num = int(input("Enter a number: "))    # ask user to input a number
+    sum += num                              # add the number to the 'sum' variable
+
+# print the current total sum of all the numbers entered so far
+print("The sum of all the numbers is:", sum)
