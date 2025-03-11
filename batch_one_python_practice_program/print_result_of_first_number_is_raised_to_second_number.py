63a08397f09e891d1ac22d8a7dbb44aa6fffe48b
cnriveraa <rivera.cn18@gmail.com>
2025-03-11 14:22:48 +0800

    ask user to input 2 numbers

---
+++ batch_one_python_practice_program/print_result_of_first_number_is_raised_to_second_number.py
@@ -0,0 +1,5 @@
+#Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
+# ask user to input 2 numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
