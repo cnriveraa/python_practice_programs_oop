c9fd9576250ac62a346c61313ebbc65b7ecffeb5
cnriveraa <rivera.cn18@gmail.com>
2025-03-12 07:45:19 +0800

    loop the remaining numbers from the list, then subtract current number to the first number

--- batch_two_python_practice_program/print_result_of_first_number_minus_all.py
+++ batch_two_python_practice_program/print_result_of_first_number_minus_all.py
@@ -5,3 +5,7 @@ numbers = [int(input(f"Enter number {i + 1}: ")) for i in range(10)]
 
 # store the first number from the list for later use
 first_number = numbers[0]
+
+# loop the remaining numbers from the list (from 2nd to last)
+for num in numbers[1:]:
+    print(f"{first_number} - {num} = {first_number - num}")
