ade31ac0b7bc4127bc7812a490127f933acfd183
cnriveraa <rivera.cn18@gmail.com>
2025-03-11 14:25:45 +0800

    print the result when the first number is raised to the second number, and use ** operator

--- batch_one_python_practice_program/print_result_of_first_number_is_raised_to_second_number.py
+++ batch_one_python_practice_program/print_result_of_first_number_is_raised_to_second_number.py
@@ -3,3 +3,8 @@
 # ask user to input 2 numbers
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+
+# print the result when the first number is raised to the second number
+# use ** operator
+result = num1 ** num2
+print(result)
