973776db9f261db949894063db9f3f48eec61303
cnriveraa <rivera.cn18@gmail.com>
2025-03-11 15:09:16 +0800

    ask the user to input 2 numbers

---
+++ batch_two_python_practice_program/print_difference_of_two_numbers.py
@@ -0,0 +1,5 @@
+#Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+
+# ask the user to input 2 numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
