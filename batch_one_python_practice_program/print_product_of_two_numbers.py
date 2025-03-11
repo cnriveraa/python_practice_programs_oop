51243c8479fec951dfa95c7c8a6bfdceeff02195
cnriveraa <rivera.cn18@gmail.com>
2025-03-11 14:11:57 +0800

    ask user to input 2 numbers and store the numbers in variables num1 and num2

---
+++ batch_one_python_practice_program/print_product_of_two_numbers.py
@@ -0,0 +1,6 @@
+#Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+
+# ask user to input 2 numbers
+# store the numbers in variables num1 and num2
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
