33e677fc888b1aaf33eaeea541094ef7aca91178
cnriveraa <rivera.cn18@gmail.com>
2025-03-12 07:52:00 +0800

    start a loop using for loop to iterate 10 times, then ask user to input a number

--- batch_two_python_practice_program/print_all_even_numbers_from_ten_numbers.py
+++ batch_two_python_practice_program/print_all_even_numbers_from_ten_numbers.py
@@ -2,3 +2,7 @@
 
 # initialize a variable 'even' to count the number of even numbers entered by the user
 even = 0
+
+# start a loop that will iterate 10 times
+for i in range(10):
+    num = int(input("Enter a number: ")) n     # ask user to input a number
