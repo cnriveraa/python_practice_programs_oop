5fd59f92aff498429fb90ce746dace56aa0c0fa8
cnriveraa <rivera.cn18@gmail.com>
2025-03-12 07:49:36 +0800

    initialize a variable 'even' to count the number of even numbers entered by the user

---
+++ batch_two_python_practice_program/print_all_even_numbers_from_ten_numbers.py
@@ -0,0 +1,4 @@
+#Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+
+# initialize a variable 'even' to count the number of even numbers entered by the user
+even = 0
