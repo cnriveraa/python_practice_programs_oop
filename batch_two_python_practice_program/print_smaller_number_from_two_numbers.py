e9c44d3f58b501aa3d4f517f6dd211d185f2c34d
cnriveraa <rivera.cn18@gmail.com>
2025-03-11 15:03:31 +0800

    check which number is smaller using if-else statement

--- batch_two_python_practice_program/print_smaller_number_from_two_numbers.py
+++ batch_two_python_practice_program/print_smaller_number_from_two_numbers.py
@@ -3,3 +3,9 @@
 # ask user to input 2 numbers
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+
+# check which number is smaller
+if num1 < num2:
+    print("The smaller number is: ", num1)
+else:
+    print("The smaller number is: ", num2)
