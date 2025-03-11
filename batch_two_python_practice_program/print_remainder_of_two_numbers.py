7169ab0e164a268306f524ef7dc5a67c473daa57
cnriveraa <rivera.cn18@gmail.com>
2025-03-12 07:37:45 +0800

    calculate the remainder of two numbers using modulo operator, then print

--- batch_two_python_practice_program/print_remainder_of_two_numbers.py
+++ batch_two_python_practice_program/print_remainder_of_two_numbers.py
@@ -3,3 +3,7 @@
 # ask user to input 2 numbers
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+
+# print the remainder when the first number is divided by the second number
+remainder = num1 % num2
+print("The remainder is: ", remainder)
