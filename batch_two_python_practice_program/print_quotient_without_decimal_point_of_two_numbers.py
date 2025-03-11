1c5929d24db614b6be6a247d443e64ce163a5cf7
cnriveraa <rivera.cn18@gmail.com>
2025-03-11 15:12:22 +0800

    ask user to input 2 numbers

---
+++ batch_two_python_practice_program/print_quotient_without_decimal_point_of_two_numbers.py
@@ -0,0 +1,5 @@
+#Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+
+# ask user to input 2 numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
