c9ba36d193f60e8521fab1eb4fc48294127adf8c
cnriveraa <rivera.cn18@gmail.com>
2025-03-12 07:42:24 +0800

    store the first number from the list for later use

--- batch_two_python_practice_program/print_result_of_first_number_minus_all.py
+++ batch_two_python_practice_program/print_result_of_first_number_minus_all.py
@@ -2,3 +2,6 @@
 
 # ask user to input 10 numbers using list comprehension
 numbers = [int(input(f"Enter number {i + 1}: ")) for i in range(10)]
+
+# store the first number from the list for later use
+first_number = numbers[0]
