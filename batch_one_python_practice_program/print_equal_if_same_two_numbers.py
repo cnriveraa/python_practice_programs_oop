ed87c83c3c93710ec99e6523333e629dd5c0e6db
cnriveraa <rivera.cn18@gmail.com>
2025-03-11 13:52:46 +0800

    If-else statement added, checking two numbers if same

--- batch_one_python_practice_program/print_equal_if_same_two_numbers.py
+++ batch_one_python_practice_program/print_equal_if_same_two_numbers.py
@@ -5,3 +5,7 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
 # check if the numbers are equal
+if num1 == num2:
+    print("Equal")
+else:
+    print("Not Equal")
