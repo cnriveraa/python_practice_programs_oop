1e14dc525234aca8cc7d98990b60d4b0ee9b4f65
cnriveraa <rivera.cn18@gmail.com>
2025-03-11 14:34:41 +0800

    start a loop using for loop and ask user to input a number

--- batch_one_python_practice_program/print_sum_of_ten_numbers.py
+++ batch_one_python_practice_program/print_sum_of_ten_numbers.py
@@ -2,3 +2,7 @@
 
 # initialize a variable 'sum' to store the total sum of the numbers
 sum = 0
+
+# start a loop that will iterate 10 times (from 0 to 9)
+for i in range(10): 
+    num = int(input("Enter a number: ")) # ask user to input a number
