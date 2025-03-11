49a86a36708d5e285a877f97222ae624a810e0a5
cnriveraa <rivera.cn18@gmail.com>
2025-03-11 13:38:47 +0800

    Comment added: ask user to input 2 numbers

---
+++ batch_one_python_practice_program/print_bigger_number_from_two_numbers.py
@@ -0,0 +1,7 @@
+#Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+
+# ask user to input 2 numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# print the bigger number
