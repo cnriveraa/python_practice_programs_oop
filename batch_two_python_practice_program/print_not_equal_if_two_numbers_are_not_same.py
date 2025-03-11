b6737fa8316cab245ff04d1a8a662d43d2c26ae1
cnriveraa <rivera.cn18@gmail.com>
2025-03-11 15:07:14 +0800

    used if-else statement to check if the number are not equal

--- batch_two_python_practice_program/print_not_equal_if_two_numbers_are_not_same.py
+++ batch_two_python_practice_program/print_not_equal_if_two_numbers_are_not_same.py
@@ -3,3 +3,9 @@
 # ask user to input 2 numbers
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+
+# check if the number are not equal
+if num1 != num2:
+    print("Not Equal")
+else:
+    print("Equal")
