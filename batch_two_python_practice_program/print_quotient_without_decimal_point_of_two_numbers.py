e19a114d66fd79a0335b66c691fb0901d6337c30
cnriveraa <rivera.cn18@gmail.com>
2025-03-11 15:13:33 +0800

    print the quotient of two numbers w/o decimal points

--- batch_two_python_practice_program/print_quotient_without_decimal_point_of_two_numbers.py
+++ batch_two_python_practice_program/print_quotient_without_decimal_point_of_two_numbers.py
@@ -3,3 +3,7 @@
 # ask user to input 2 numbers
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+
+# print the quotient w/o decimal point
+quotient = num1 // num2
+print("The quotient is: ", quotient)
