edce6f184507de1eb6d36b5b1ed151cd380dbe38
cnriveraa <rivera.cn18@gmail.com>
2025-03-12 14:07:41 +0800

    added pseudocode

---
+++ batch_three_python_practice_program/check_unique_and_duplicate_numbers_from_user_input.py
@@ -0,0 +1,9 @@
+# Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
+
+# initialize an empty set to store the numbers
+# loop until invalid input is received
+# ask user to input a number
+# check if the number is already in the set
+# add unique number to the set
+# if input is not integer, print error message
+# exit loop if invalid input
