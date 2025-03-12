a351390ff12205dfd4a5dfc80c00568619bf4b8b
cnriveraa <rivera.cn18@gmail.com>
2025-03-12 23:24:35 +0800

    comments added for code readability

--- batch_four_python_practice_program/print_highest_number.py
+++ batch_four_python_practice_program/print_highest_number.py
@@ -7,11 +7,11 @@ numbers = []
 while True:
     try:
         num = int(input("Enter a number: "))
-        numbers.append(num)
+        numbers.append(num)   # add numbers to the list
     except ValueError:
-        print("Invalid input.")
+        print("Invalid input.")  # print error message if input is not integer
         break
 
 # display the highest number
-if num in numbers:
+if num in numbers:    # check if the list is not empty
     print(f"The highest number is {max(numbers)}.")
