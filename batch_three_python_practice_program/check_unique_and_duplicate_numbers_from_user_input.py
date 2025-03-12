39ec0a548dda12123b5d52b4e9fcb38889990c86
cnriveraa <rivera.cn18@gmail.com>
2025-03-12 14:08:31 +0800

    initialize an empty set to store the numbers

--- batch_three_python_practice_program/check_unique_and_duplicate_numbers_from_user_input.py
+++ batch_three_python_practice_program/check_unique_and_duplicate_numbers_from_user_input.py
@@ -1,6 +1,8 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
 
 # initialize an empty set to store the numbers
+numbers = set()
+
 # loop until invalid input is received
 # ask user to input a number
 # check if the number is already in the set
