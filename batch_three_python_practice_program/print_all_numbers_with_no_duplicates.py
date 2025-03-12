37b82e98729db7cf2f0f6000d635f66c56ed6ce1
cnriveraa <rivera.cn18@gmail.com>
2025-03-12 13:19:55 +0800

    initialize an empty list to store numbers

---
+++ batch_three_python_practice_program/print_all_numbers_with_no_duplicates.py
@@ -0,0 +1,4 @@
+# Prog01: Create a program that asks the user to input 10 numbers and displays all numbers that don't have duplicates.
+
+# initialize an empty list to store the numbers
+numbers = []
