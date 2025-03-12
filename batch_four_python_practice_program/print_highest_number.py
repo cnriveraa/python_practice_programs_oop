99101c4ca2e00860e4509c0682f2eb6216862e97
cnriveraa <rivera.cn18@gmail.com>
2025-03-12 23:20:15 +0800

    initialize an empty list to store numbers

--- batch_four_python_practice_program/print_highest_number.py
+++ batch_four_python_practice_program/print_highest_number.py
@@ -1,5 +1,7 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number.
 
 # initialize an empty list to store numbers
+numbers = []
+
 # ask user to input numbers
 # display the highest number
