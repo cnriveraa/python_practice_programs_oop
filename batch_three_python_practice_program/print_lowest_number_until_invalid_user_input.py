05debc7350373b6c2eda4890f4c5a8e15cc04f75
cnriveraa <rivera.cn18@gmail.com>
2025-03-12 14:21:59 +0800

    added pseudocode

---
+++ batch_three_python_practice_program/print_lowest_number_until_invalid_user_input.py
@@ -0,0 +1,7 @@
+# Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number.
+
+# initialize an empty list to store the number
+# loop until invalid input is received
+# ask user to input a number
+# error message if non-integer input
+# display lowest number
