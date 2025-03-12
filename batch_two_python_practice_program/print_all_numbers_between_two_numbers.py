16356dfa9caaaf553a8093ba2fe63d17122c8e98
cnriveraa <rivera.cn18@gmail.com>
2025-03-12 08:18:20 +0800

    ask user to input 2 numbers

---
+++ batch_two_python_practice_program/print_all_numbers_between_two_numbers.py
@@ -0,0 +1,5 @@
+#Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+
+# ask user to input 2 numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
