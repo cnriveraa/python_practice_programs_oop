9fb28aa0af78fd51e4d2a4fef37076ed30f9834e
cnriveraa <rivera.cn18@gmail.com>
2025-03-12 23:36:12 +0800

    pseudocode added

---
+++ batch_four_python_practice_program/print_average_from_all_number_inputs.py
@@ -0,0 +1,5 @@
+# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+
+# initialize an empty list to store numbers
+# start loop to iterate numbers
+# find the average
