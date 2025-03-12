a25434340b2816f29e04e1e6f385b4e8329230a3
cnriveraa <rivera.cn18@gmail.com>
2025-03-12 14:35:52 +0800

    initialize an empty list to store the numbers

--- batch_three_python_practice_program/print_lowest_to_highest_numbers.py
+++ batch_three_python_practice_program/print_lowest_to_highest_numbers.py
@@ -1,6 +1,8 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function.
 
 # initialize an empty list to store the numbers
+numbers = []
+
 # loop until invalid input is received
 # ask the user to input a number
 # add the number to the list
