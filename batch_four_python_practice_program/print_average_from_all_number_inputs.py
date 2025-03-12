61f78839a40229251ef8c633c7d73a78d2097254
cnriveraa <rivera.cn18@gmail.com>
2025-03-12 23:36:30 +0800

    initialize an empty list to store numbers

--- batch_four_python_practice_program/print_average_from_all_number_inputs.py
+++ batch_four_python_practice_program/print_average_from_all_number_inputs.py
@@ -1,5 +1,7 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
 
 # initialize an empty list to store numbers
+numbers = []
+
 # start loop to iterate numbers
 # find the average
