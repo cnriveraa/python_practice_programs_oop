0b7a29a323e96846c876825a59ee314ab436577c
cnriveraa <rivera.cn18@gmail.com>
2025-03-12 14:35:04 +0800

    added pseudocode

---
+++ batch_three_python_practice_program/print_lowest_to_highest_numbers.py
@@ -0,0 +1,9 @@
+# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function.
+
+# initialize an empty list to store the numbers
+# loop until invalid input is received
+# ask the user to input a number
+# add the number to the list
+# if input is not integer, print error message
+# sort the numbers from lowest to highest
+# display the numbers from lowest to highest
