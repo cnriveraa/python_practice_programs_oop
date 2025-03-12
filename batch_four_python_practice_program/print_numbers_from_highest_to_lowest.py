2154b28b61fa93581725f2b28db881f8242b7391
cnriveraa <rivera.cn18@gmail.com>
2025-03-12 23:28:00 +0800

    pseudocode added

---
+++ batch_four_python_practice_program/print_numbers_from_highest_to_lowest.py
@@ -0,0 +1,6 @@
+# Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function.
+
+# initialize an empty list to store numbers
+# ask user to input numbers
+# sort the numbers from highest to lowest
+# display the number from highest to lowest
