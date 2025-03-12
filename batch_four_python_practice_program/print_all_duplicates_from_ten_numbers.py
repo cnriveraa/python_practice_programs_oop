2d9b09916a42b0ffe91cdb42a799211202ae2799
cnriveraa <rivera.cn18@gmail.com>
2025-03-12 22:54:16 +0800

    initialize an empty set to store numbers

--- batch_four_python_practice_program/print_all_duplicates_from_ten_numbers.py
+++ batch_four_python_practice_program/print_all_duplicates_from_ten_numbers.py
@@ -1,6 +1,8 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 
 # initialize an empty set to store numbers
+numbers = []
+
 # ask user to input 10 numbers
 # initialize a set to track duplicates
 # find duplicates
