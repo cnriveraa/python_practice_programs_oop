748e337eea8a9821056e039317b8d47d8b49ae67
cnriveraa <rivera.cn18@gmail.com>
2025-03-12 13:56:07 +0800

    used while loop to receive valid input

--- batch_three_python_practice_program/print_all_numbers_and_first_entry_of_duplicates.py
+++ batch_three_python_practice_program/print_all_numbers_and_first_entry_of_duplicates.py
@@ -4,3 +4,6 @@ numbers = []
 
 # ask user to input 10 numbers
 for i in range(10):
+    while True:  # loop until valid input is received
+        try: 
+            num = int(input(f"Enter number {i + 1}: "))
