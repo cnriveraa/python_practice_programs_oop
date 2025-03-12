df638ddd23cefb7314326e4d0183c6eac9d26dac
cnriveraa <rivera.cn18@gmail.com>
2025-03-12 08:20:47 +0800

    used for loop, then print all numbers between two numbers

--- batch_two_python_practice_program/print_all_numbers_between_two_numbers.py
+++ batch_two_python_practice_program/print_all_numbers_between_two_numbers.py
@@ -3,3 +3,7 @@
 # ask user to input 2 numbers
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+
+# loop the numbers from num1 + 1, not including num2
+for i in range(num1 + 1, num2):
+    print(i)
