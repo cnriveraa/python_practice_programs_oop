11729df7cffa992f87510b79b4f356965fba2612
cnriveraa <rivera.cn18@gmail.com>
2025-03-12 14:10:12 +0800

    used while loop until invalid input is received, and asked user to input a number

--- batch_three_python_practice_program/check_unique_and_duplicate_numbers_from_user_input.py
+++ batch_three_python_practice_program/check_unique_and_duplicate_numbers_from_user_input.py
@@ -4,7 +4,10 @@
 numbers = set()
 
 # loop until invalid input is received
-# ask user to input a number
+while True:
+    try: 
+        num = int(input("Enter a number: "))    # ask user to input a number
+
 # check if the number is already in the set
 # add unique number to the set
 # if input is not integer, print error message
